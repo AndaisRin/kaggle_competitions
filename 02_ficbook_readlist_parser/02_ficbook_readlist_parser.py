76509cefa1347e0e3226ef0d36f6eab3673a4dae
AndaisRin <32798810+AndaisRin@users.noreply.github.com>
2024-05-06 23:02:43 +0300

    Update 02_ficbook_readlist_parser.py
    Изменения:
    - программа корректно отрабатывает работы в статусе "В процессе" и "Заморожено";
    - файл сохраняется с указанием даты выгрузки;
    - настроено завершение программы.

--- 02_ficbook_readlist_parser/02_ficbook_readlist_parser.py
+++ 02_ficbook_readlist_parser/02_ficbook_readlist_parser.py
@@ -14,23 +14,30 @@
 ### Шаг 1. Установка необходимых библиотек
 '''
 # !pip install undetected-chromedriver
-# Модули для работы с временем
-import time  # для работы с временем
 
-# Модули для работы с данными
+# Импорты для работы с временем
+import time
+import datetime
+
+# Импорты для работы с данными
 import pandas as pd
+import re
 
-# Модули для управления браузером и взаимодействия с веб-страницами
-import undetected_chromedriver as uc
+# Импорты для управления браузером и взаимодействия с веб-страницами
+import undetected_chromedriver as uc  # Незамеченный драйвер Chrome
+from selenium.webdriver.common.by import By  # Для выбора элементов на веб-странице
+from selenium.webdriver.support.ui import WebDriverWait  # Для ожидания появления элементов
+from selenium.webdriver.support import expected_conditions as EC  # Для ожидаемых условий
+from selenium.webdriver.common.keys import Keys  # Для ввода клавиш на веб-страницах
 
-from selenium.webdriver.common.by import By  # для выбора элементов на веб-странице
-from selenium.webdriver.support.ui import WebDriverWait  # для ожидания появления элементов
-from selenium.webdriver.support import expected_conditions as EC  # для ожидаемых условий
-from selenium.webdriver.common.keys import Keys
+# Импорты для управления программой и выхода из нее
+import sys
 
 '''
 ### Шаг 2. Подготовка к выполнению задачи, создание необходимых функций
 '''
+
+
 # Вход в учетную запись и переход на страницу прочитанного
 def login_and_continue(url, username, password):
     options = uc.ChromeOptions()
@@ -118,10 +125,16 @@ def get_fanfics(driver):
         except:
             pass
         if pages_element:
-            pages_text = pages_element.text.split(',')[0]
-            pages_number = int(pages_text.split()[0])
+            pages_text = pages_element.text
+            # Используем регулярное выражение для поиска числа
+            pages_match = re.search(r'\b\d+\b', pages_text)
+            if pages_match:
+                pages_number = int(pages_match.group())
+            else:
+                pages_number = None
         else:
             pages_number = None
+
         fandom = element.find_element(By.XPATH, './/dl[@class="fanfic-inline-info"]/dd/a').text
         date_element = element.find_element(By.XPATH, './/div[@class="read-notification"]//span[@title]')
         date = date_element.get_attribute("title")
@@ -147,28 +160,35 @@ def get_fanfics(driver):
 '''
 ### Шаг 3. Подготовка и сбор данных
 '''
-url = "https://ficbook.net/"
+try:
+    url = "https://ficbook.net/"
 
-username = str(input('Введите логин:'))
-password = str(input('Введите пароль:'))
-end_page = int(input('Сколько страниц надо выгрузить:'))
+    username = str(input('Введите логин:'))
+    password = str(input('Введите пароль:'))
+    end_page = int(input('Сколько страниц надо выгрузить:'))
 
-driver = login_and_continue(url, username, password)
+    driver = login_and_continue(url, username, password)
 
-df = get_fanfics(driver)
-for i in range(1, end_page):
-    # Переход на следующую страницу
-    next_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '//div[@class="page-arrow page-arrow-next"]/a')))
-    next_button.click()
-    time.sleep(2)
+    df = get_fanfics(driver)
+    for i in range(1, end_page):
+        # Переход на следующую страницу
+        next_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//div[@class="page-arrow page-arrow-next"]/a')))
+        next_button.click()
+        time.sleep(2)
+        # Получаем данные с текущей страницы
+        current_page_df = get_fanfics(driver)
 
-    df = pd.concat([df, get_fanfics(driver)], ignore_index=True)
+        # Объединяем данные с предыдущими страницами
+        df = pd.concat([df, current_page_df], ignore_index=True)
 
-'''
-### Шаг 4. охранение полученных результатов и завершение программы
-'''
-# Save the filtered DataFrame to an Excel file
-df.to_excel('Readlist_ficbook_{}.xlsx'.format(username), index=False)
-
-driver.quit()
+    '''
+    ### Шаг 4. Сохранение полученных результатов и завершение программы
+    '''
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    df.to_excel('Readlist_ficbook_{}_{}.xlsx'.format(username, current_date), index=False)
+finally:
+    # Закрытие браузера
+    driver.quit()
+    # Завершение работы программы
+    sys.exit()
